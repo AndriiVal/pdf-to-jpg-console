6b0bf9ec018278308d2e361d8825efca37c463f4
Andrii Valchuk <55961471+AndriiVal@users.noreply.github.com>
2020-04-11 20:52:06 +0300

    Create pdf_to_jpg.py

---
+++ pdf_to_jpg.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
+# Author: Andrii Valchuk
+
+from wand.image import Image as wimg
+from progress.bar import IncrementalBar
+
+pdfsource = input("Enter source pdf file: ")
+imgresolution = int(input("Enter resolution by output images: "))
+
+print("Read pdf file, pleas wait...")
+
+pdf = wimg(filename=pdfsource, resolution=imgresolution)
+pdfimage = pdf.convert("jpeg")
+imgsequence = pdfimage.sequence
+bar = IncrementalBar('Convert', max = len(imgsequence))
+i = 1
+for img in imgsequence:
+    bar.next()
+    page = wimg(image=img)
+    page.save(filename=str(i)+".jpg")
+    i += 1
+
+bar.finish()
+
+print("Finish\n")
