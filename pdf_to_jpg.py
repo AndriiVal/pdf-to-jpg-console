9dc8abf6fd4540672124f465cc9c160995c4befa
Andrii Valchuk <55961471+AndriiVal@users.noreply.github.com>
2020-04-12 23:41:46 +0300

    Updte pdf_to_jpg.py
    Add exceptions

--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -11,17 +11,20 @@ imgresolution = int(input("Enter resolution by output images: "))
 
 print("Read pdf file, pleas wait...")
 
-pdf = wimg(filename=pdfsource, resolution=imgresolution)
-pdfimage = pdf.convert("jpeg")
-imgsequence = pdfimage.sequence
-bar = IncrementalBar('Convert', max = len(imgsequence))
-i = 1
-for img in imgsequence:
-    bar.next()
-    page = wimg(image=img)
-    page.save(filename=str(i)+".jpg")
-    i += 1
-
-bar.finish()
-
-print("Finish\n")
+try:
+    pdf = wimg(filename=pdfsource, resolution=imgresolution)
+    pdfimage = pdf.convert("jpeg")
+    imgsequence = pdfimage.sequence
+    bar = IncrementalBar('Convert', max = len(imgsequence))
+    i = 1
+    for img in imgsequence:
+        bar.next()
+        page = wimg(image=img)
+        page.save(filename=str(i)+".jpg")
+        i += 1
+    bar.finish()
+    print("Finish\n")
+except PolicyError as ex:
+    print('Pdf extraction forbidden by Imagemagick policy: %s', ex)
+except Exception as ex:
+    print('Cannot extract image: %s', ex)
